89b7b5eaceb2abe360903905bb150d8d9795d989
Djordje Knezevic <starwarrior@mycity.rs>
2022-04-16 10:49:49 +0200

    Refactor code

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
+ 
   function fetchMoviesHandler() {
     fetch('https://swapi.dev/api/films/').then(response => {
       return response.json();
